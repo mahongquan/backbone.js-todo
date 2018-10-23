df00728c6d76c5a7eb7da417e068f440e03bc014
mahongquan <mahongquan@sina.com>
2018-10-23 13:40:23 +0800

    electron react material-ui

---
+++ src/index_local.js
@@ -0,0 +1,8 @@
+//use with local babel-register
+//import App from './AppRoutes';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './todos_mui';
+ReactDOM.render(
+  <App />,  document.getElementById('root')
+);
